da6eade447b0f1a4cf31486fbe8d6b4c05f6d1ef
SiroiluT <tulio-evangelista@outlook.com>
2025-11-27 01:14:22 -0300

    feat: cria Beans para adicionar contexto aos Usecases sem que ele dependa do controller

---
+++ src/main/java/clean_architecture/GerenciamentoSalasClean/infra/beans/BeanConfiguration.java
@@ -0,0 +1,18 @@
+package clean_architecture.GerenciamentoSalasClean.infra.beans;
+
+
+import clean_architecture.GerenciamentoSalasClean.core.gateway.SalaGatway;
+import clean_architecture.GerenciamentoSalasClean.core.usecases.CriarReservaCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public CriarReservaCaseImpl criarReservaCaseImpl(SalaGatway salaGatway) {
+        return new CriarReservaCaseImpl(salaGatway);
+    }
+
+
+}
